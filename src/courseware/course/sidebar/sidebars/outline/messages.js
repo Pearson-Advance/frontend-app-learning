4e50c7c6b3bc930de6675c9d303cdd31ee05618b
Felipe B <andres.bermudez@edunext.co>
2022-12-22 12:45:48 +0100

    feat: add Outline Navigation Sidebar.

---
+++ src/courseware/course/sidebar/sidebars/outline/messages.js
@@ -0,0 +1,16 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  outlineTitle: {
+    id: 'outline.sidebar.title',
+    defaultMessage: 'Outline',
+    description: 'Title text for a outline sidebar where users are able to navigate the course',
+  },
+  openOutlineTrigger: {
+    id: 'outline.sidebar.open.button',
+    defaultMessage: 'Show outline sidebar',
+    description: 'Alt text for a button that opens the outline sidebar feature',
+  },
+});
+
+export default messages;
